77f5f7c31cb633868860133e25b541c3a2d23072
sycDev <sinyi.20028@gmail.com>
2023-06-29 14:08:19 +0800

    unit testing for jpa repo create bid & listing

---
+++ src/test/java/com/wou/hifiusedcar/repository/ListingRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.wou.hifiusedcar.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.wou.hifiusedcar.entity.Listing;
+import com.wou.hifiusedcar.entity.User;
+
+@DataJpaTest
+@Rollback(true)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ListingRepositoryTest {
+
+	@Autowired
+	private ListingRepository listingRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Test
+	public void testCreateListing() {
+		// Listing details
+		String make = "Mercedes-Benz";
+		String model = "E55 AMG";
+		Integer regYear = 2000;
+		String image = "mercedes1.jpg";
+		Integer milleage = 55400;
+		String description = "The W210-generation Mercedes-Benz E55 AMG is settling nicely into its new role as a young classic – "
+				+ "not many of these were made, well-kept ones are becoming difficult to find, and values keep going up. I still have "
+				+ "tremendously fond memories of the one I owned ten years ago, and I truly wish I had never sold it. This particular "
+				+ "E55 AMG touts low mileage for one of these, a gorgeous two-tone interior, and no modifications. Plus, it comes with "
+				+ "a clean, accident-free Carfax report – AND it's offered with the added excitement of no reserve.";
+		BigDecimal minPrice = new BigDecimal("30000.00");
+		String dateTimeString = "2023-06-30T12:00:00";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+		LocalDateTime endTime = LocalDateTime.parse(dateTimeString, formatter);
+		String status = "Active";
+		// Retrieve the User account from database
+		User seller = userRepository.findByEmail("alvin@gmail.com").get();
+		
+		// Create listing
+		Listing listing = new Listing();
+		listing.setMake(make);
+		listing.setModel(model);
+		listing.setRegYear(regYear);
+		listing.setImage(image);
+		listing.setMilleage(milleage);
+		listing.setDescription(description);
+		listing.setMinPrice(minPrice);
+		listing.setEndTime(endTime);
+		listing.setStatus(status);
+		listing.setUser(seller);
+
+		// Save the listing to database
+		Listing listingToCreate = listingRepository.save(listing);
+		
+		// Retrieve the listing from database
+		Listing listingSaved = listingRepository.findById(listingToCreate.getListingId()).get();
+		
+		// Verify that the listing record is successfully saved
+		assertThat(listingToCreate.getListingId()).isEqualTo(listingSaved.getListingId());
+	}
+}
