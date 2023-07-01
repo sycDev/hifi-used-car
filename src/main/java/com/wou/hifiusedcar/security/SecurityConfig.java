c910f426ece668eb2a1d07d9a02bf4895379b53a
sycDev <sinyi.20028@gmail.com>
2023-07-01 23:19:19 +0800

    fix #1: use WebSecurityConfigurerAdapter instead

--- src/main/java/com/wou/hifiusedcar/security/SecurityConfig.java
+++ src/main/java/com/wou/hifiusedcar/security/SecurityConfig.java
@@ -3,17 +3,18 @@ package com.wou.hifiusedcar.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public UserDetailsService userDetailsService() {
 		return new UserDetailsServiceImpl();
@@ -24,30 +25,25 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		
-		authProvider.setUserDetailsService(userDetailsService());
-		authProvider.setPasswordEncoder(passwordEncoder());
-		
-		return authProvider;
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService())
+        	.passwordEncoder(passwordEncoder());
 	}
 	
-	@Bean
-	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		http
-			.sessionManagement()
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
 				.maximumSessions(1)
 				.expiredUrl("/signin?expired=true");
 		
-		http
-			.authorizeRequests()
+		http.authorizeRequests()
 				.antMatchers("/assets/**").permitAll()
-        .antMatchers("/css/**").permitAll()
-        .antMatchers("/js/**").permitAll()
-        .antMatchers(HttpMethod.GET, "/").permitAll()
+				.antMatchers("/css/**").permitAll()
+				.antMatchers("/js/**").permitAll()
+
+				.antMatchers(HttpMethod.GET, "/").permitAll()
 				.antMatchers(HttpMethod.GET, "/about").permitAll()
 				.antMatchers(HttpMethod.GET, "/contact").permitAll()
 				.antMatchers(HttpMethod.GET, "/terms-of-use").permitAll()
@@ -55,12 +51,15 @@ public class SecurityConfig {
 				.antMatchers(HttpMethod.GET, "/error").permitAll()
 				.antMatchers(HttpMethod.GET, "/signup").permitAll()
 				.antMatchers(HttpMethod.POST, "/register").permitAll()
-				.antMatchers(HttpMethod.GET, "/sell-car", "/my-listings").hasAuthority("USER")
-        .antMatchers(HttpMethod.POST, "/listing/add", "/listing/updateStatus").hasAuthority("USER")
-				.antMatchers(HttpMethod.POST, "/bid/add").hasAuthority("USER")
-				.antMatchers(HttpMethod.GET, "/auction", "/past-auction", "/listing/**", "/search").hasAnyAuthority("USER", "ADMIN")
-				.antMatchers(HttpMethod.GET, "/dashboard/**").hasAuthority("ADMIN")
-				.antMatchers(HttpMethod.POST, "/dashboard/**").hasAuthority("ADMIN")
+
+				.antMatchers(HttpMethod.GET, "/sell-car", "/my-listings").hasRole("USER")
+				.antMatchers(HttpMethod.POST, "/listing/add", "/listing/updateStatus").hasRole("USER")
+				.antMatchers(HttpMethod.POST, "/bid/add").hasRole("USER")
+
+				.antMatchers(HttpMethod.GET, "/auction", "/past-auction", "/listing/**", "/search").hasAnyRole("USER", "ADMIN")
+
+				.antMatchers(HttpMethod.GET, "/dashboard/**").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/dashboard/**").hasRole("ADMIN")
 				.and()
 			.formLogin()
 				.loginPage("/signin")
@@ -76,7 +75,5 @@ public class SecurityConfig {
 				.logoutUrl("/logout")
 				.logoutSuccessUrl("/signin?logout")
 				.permitAll();
-		
-		return http.build();
 	}
 }
--- src/main/java/com/wou/hifiusedcar/security/UserDetailsServiceImpl.java
+++ src/main/java/com/wou/hifiusedcar/security/UserDetailsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.wou.hifiusedcar.security;
 
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,21 +13,33 @@ import com.wou.hifiusedcar.entity.User;
 import com.wou.hifiusedcar.repository.UserRepository;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
-
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		Optional<User> savedUser = userRepository.findByEmail(email);
+
 		if (!savedUser.isPresent()) {
             throw new UsernameNotFoundException("Login user " + email + " is not valid. Please re-enter.");
         }
 		
-		for (Role role: savedUser.get().getRoles()) {
-			System.out.println("Role: " + role.getRoleName());
+		org.springframework.security.core.userdetails.User.UserBuilder userBuilder = 
+        		org.springframework.security.core.userdetails.User.builder();
+		
+		User user = savedUser.get();
+        Set<Role> roles = user.getRoles();
+        String[] roleNames = roles.stream()
+								.map(Role::getRoleName)
+								.toArray(String[]::new);
+		
+        for (String role : roleNames) {
+        	System.out.println("Role Name is " + role);
 		}
 
-		return new CustomUserDetails(savedUser.get());
+		return userBuilder.username(user.getEmail())
+        		.password(user.getPassword())
+        		.roles(roleNames)
+        		.build();
 	}
 }
